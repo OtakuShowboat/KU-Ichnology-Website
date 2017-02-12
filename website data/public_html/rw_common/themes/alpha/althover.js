e403475c73bc72e311d9d67cc4c3825149d44fdf
OtakuShowboat <joshuadhogue@ymail.com>
2017-02-12 01:44:27 -0600

    Massive Code Update - Feb 9, 10, 12, 2017
    Tons of changes. All pages now use header.html and footer.html (thanks
    to N++ regular expressions!) All videos are converted to .mp4 and
    compressed. Video pages code updated to use video tags. Ants burrowing
    video (broken) replaced with ants burrowing through tunnel wall video.
    Fixed 2 3D picture pages by converting the .tifs to .jpgs. Student and
    Alumni pages updated, including two pictures added. Alumni and Contact
    pages now accessible via navbar. Navbar script updated so all parents
    are highlighted regardless of the page you're on.

--- website data/public_html/rw_common/themes/alpha/althover.js
+++ website data/public_html/rw_common/themes/alpha/althover.js
@@ -1,10 +1,19 @@
 //<![CDATA[
+
+//Not the most elegant of code, but it gets the job done. Sets a class of "current" to the link on the navbar for the current page and up to two parents (e.g. if on the Ants Scraping video page, the Videos, Tracemaking Experiments, and Ants Scraping link list elements will have a class of "current" and get highlighted by colourtag.css, currently set to White (#FFFFFF). Code originally from a separate blog hosting site, but modified to add the second level check and the homepage path check so that "KU Ichnology" is highlighted when first entering the site.
+
+var homeLink = document.getElementById("homepage");
+if(window.location.pathname == "/") //Checks if the path is empty (i.e. you are on the home URL: ichnology.ku.edu and not on another page such as ichnology.ku.edu/tracefossils.html).
+	{
+		homeLink.className += "current";
+	};
  function setActiveLink(currentUrl) {
   var activeClass = 'current';
   var navLinks = document.querySelectorAll('#navcontainer a'); //all links inside the nav
   for (var i = 0, l = navLinks.length; i < l; i++) {
    var link = navLinks[i];
    var url = link.getAttribute('href');
+
    if(currentUrl == url) {
     if (link.classList) {
      link.classList.add(activeClass);
@@ -34,11 +43,49 @@
     }
     if (sectionLink.classList) {
      sectionLink.classList.add(activeClass);
-    } else {
+	 if (listitem.parentNode.parentNode && listitem.parentNode.parentNode.parentNode) { //if parent of the parent <li /> exists and has parent <ul>
+		var listParent = listitem.parentNode.parentNode.parentNode;
+			if (!(listParent.getAttribute('id'))) {
+				var topParentItem = listParent.parentNode;
+				var childrenParent = topParentItem.children;
+				var sectionParentLink = {};
+				for (var i = 0, l = childrenParent.length; i < l; i++) { //loop through children and get first one - should be the section link
+				var childParent = childrenParent[i];
+					if(childParent.nodeType == 1) {
+					sectionLinkParent = childParent;
+					break;
+					}
+				}
+				if (sectionLink.classList) {
+				sectionLinkParent.classList.add(activeClass);
+				} else {sectionLinkParent.className += ' ' + activeClass;					
+				}
+			}
+	 }
+	} else {
      sectionLink.className += ' ' + activeClass;
+	 if (listitem.parentNode.parentNode && listitem.parentNode.parentNode.parentNode) { //if parent of the parent <li /> exists and has parent <ul>
+		var listParent = listitem.parentNode.parentNode.parentNode;
+			if (!(listParent.getAttribute('id'))) {
+				var topParentItem = listParent.parentNode;
+				var childrenParent = topParentItem.children;
+				var sectionParentLink = {};
+				for (var i = 0, l = childrenParent.length; i < l; i++) { //loop through children and get first one - should be the section link
+				var childParent = childrenParent[i];
+					if(childParent.nodeType == 1) {
+					sectionLinkParent = childParent;
+					break;
+					}
+				}
+				if (sectionLink.classList) {
+				sectionLinkParent.classList.add(activeClass);
+				} else {sectionLinkParent.className += ' ' + activeClass;					
+				}
+			}
     }
    }
   }
+ }
  }
   
  function ready(fn) {
@@ -48,6 +95,8 @@
    document.addEventListener('DOMContentLoaded', fn);
   }
  }
+ 
+
  
  function runApplication() {
   //console.log(window.location.pathname);
