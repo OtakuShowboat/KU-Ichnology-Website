6cd3cc31de0c621ca3320ab4f7416f00957c6d1c
OtakuShowboat <joshuadhogue@ymail.com>
2017-02-22 18:22:43 -0600

    No caching during development
    Due to the rapid changes made to the site, I have disabled caching. A
    simple page reload will fix most issues when updates go live, though
    clearing the browser cache or a hard reload should always work.

--- website data/public_html/result.php
+++ website data/public_html/result.php
@@ -2,11 +2,28 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+  <meta name="robots"
+        content="all" />
 <title>Results</title>
+<link rel="stylesheet"
+        type="text/css"
+        media="screen"
+        href="/rw_common/themes/alpha/styles.css" />
+        <!--[if IE 6]><link rel="stylesheet" type="text/css" media="screen" href="/rw_common/themes/alpha/ie6.css"  /><![endif]-->
+        <!--[if IE 6]><style type="text/css" media="screen">body {behavior: url(/rw_common/themes/alpha/csshover.htc);}</style><![endif]-->
+
+<script type="text/javascript"
+      src="/rw_common/themes/alpha/javascript.js">
+</script>
+<script> 
+$(function(){
+  $("#header").load("/header.html"); 
+  $("#footer").load("/footer.html"); 
+});
+</script>
 </head>
 
 <body>
-
 <?php
 // Your code here to handle a successful verification
 $ContactName = $_POST['ContactName'];
@@ -24,13 +41,32 @@ $message = "Name: $ContactName\n"
 mail($email_to, $subject ,$message ,$header ) ;
 
 ?>
+<div id="container">
+    <!-- Start container -->
+	<div id="header"></div>
+    <div id="contentContainer">
+      <!-- Start main content wrapper -->
 
-
+      <div id="content">
+        <!-- Start content -->
 <h1>Thank You for Your Comments</h1>
 
 <p>Your information has been sent</p>
 
 <p>Thank you for your interest in the IchnoBioGeoSciences Research Group</p> 
+		<br />
+		</span>
+      </div><!-- End content -->
+    </div><!-- End main content wrapper -->
+
+    <div class="clearer"></div>
+
+    <div id="footer"></div>
+	</div>
+
+
+
+
 
 </body>
 </html>
